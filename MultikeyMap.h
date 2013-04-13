cc853ff291f91180d7f0fffb9231291e7c698c36
pilardi <CodePi@users.noreply.github.com>
2013-04-13 02:42:35 +0000

    rename

---
+++ MultikeyMap.h
@@ -0,0 +1,55 @@
+#include <map>
+#include <stdexcept>
+
+template <typename K1, typename K2, typename ValType>
+class MultikeyMap{
+public:
+	typedef std::pair<K1,K2> KeyPair;
+	typedef std::pair<const KeyPair, ValType> PairPair;
+
+	void insert(const K1& k1, const K2& k2, const ValType& val){
+		KeyPair kp = make_pair(k1,k2);
+		map1[k1] = kp;
+		map2[k2] = kp;
+		mainMap[kp] = val;
+	}
+
+	PairPair get1(const K1& k1){
+		return *mainMap.find(map1.at(k1));
+	}
+
+	PairPair get2(const K2& k2){
+		return *mainMap.find(map2.at(k2));
+	}
+
+	int count1(const K1& k1){
+		return map1.count(k1);
+	}
+
+	int count2(const K2& k2){
+		return map2.count(k2);
+	}
+
+	void erase(const KeyPair& kp){
+		map1.erase(kp.first);
+		map2.erase(kp.second);
+		mainMap.erase(kp);
+	}
+
+	void erase1(const K1& k1){
+		if(!map1.count(k1)) return;
+		erase(map1[k1]);
+	}
+
+	void erase2(const K2& k2){
+		if(!map2.count(k2)) return;
+		erase(map2[k2]);
+	}
+
+private:
+	std::map<KeyPair, ValType> mainMap;
+	std::map<K1, KeyPair>      map1;
+	std::map<K2, KeyPair>      map2;
+
+};
+
---
+++ MultikeyMap_test.cpp
@@ -0,0 +1,31 @@
+#include "MultikeyMap.h"
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	MultikeyMap<int, int, int> mmap;
+
+	mmap.insert(1,2,3);
+	mmap.insert(4,5,6);
+	mmap.insert(7,8,9);
+
+	cout << mmap.get1(1).second << endl;
+	cout << mmap.get1(4).second << endl;
+	cout << mmap.get1(7).second << endl;
+	cout << mmap.get2(2).second << endl;
+	cout << mmap.get2(5).second << endl;
+	cout << mmap.get2(8).second << endl;
+	cout << mmap.count1(1) << endl;
+	cout << mmap.count1(2) << endl;
+	cout << mmap.count2(1) << endl;
+	cout << mmap.count2(2) << endl;
+
+	try{
+		mmap.get1(10);
+	}catch(...){
+		cout << "Expected exception caught\n";
+	}
+
+	getchar();
+}
