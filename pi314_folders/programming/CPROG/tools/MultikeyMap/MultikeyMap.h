264146f843f6fc730bece47ee9e104fdec8d1923
pilardi <pilardi@68f32d55-edf0-144f-a607-5c5f177388a3>
2013-04-14 19:58:44 +0000

    - prevented dups in get methods

--- pi314_folders/programming/CPROG/tools/MultikeyMap/MultikeyMap.h
+++ pi314_folders/programming/CPROG/tools/MultikeyMap/MultikeyMap.h
@@ -42,11 +42,18 @@ public:
 	std::vector<iterator> get1(const Key1& key1){
 		vector<iterator> vec;
 		typename std::multimap<Key1, KeyPair>::iterator i;
+		KeyPair* lastKp = NULL;
 		for(i = map1.lower_bound(key1); i != map1.upper_bound(key1); i++){
 			KeyPair& kp = i->second;
+
+			// if kp same as last, skip
+			if(lastKp && kp==*lastKp) continue;
+
+			// push_back iterator for each match
 			for(iterator j = mainMap.lower_bound(kp); j != mainMap.upper_bound(kp); j++){
 				vec.push_back(j);
 			}
+			lastKp = &kp;
 		}
 		return vec;
 	}
@@ -55,11 +62,18 @@ public:
 	std::vector<iterator> get2(const Key2& key2){
 		vector<iterator> vec;
 		typename std::multimap<Key2, KeyPair>::iterator i;
+		KeyPair* lastKp = NULL;
 		for(i = map2.lower_bound(key2); i != map2.upper_bound(key2); i++){
 			KeyPair& kp = i->second;
+
+			// if kp same as last, skip
+			if(lastKp && kp==*lastKp) continue;
+
+			// push_back iterator for each match
 			for(iterator j = mainMap.lower_bound(kp); j != mainMap.upper_bound(kp); j++){
 				vec.push_back(j);
 			}
+			lastKp = &kp;
 		}
 		return vec;
 	}
